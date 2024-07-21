aa2d7581558cc63aef506765d50c167cae5791c6
nailsonseat <aadarshv3@gmail.com>
2024-07-21 11:33:34 +0530

    Setup fastapi

---
+++ server/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from resources.test import root_router
+import uvicorn
+
+
+app = FastAPI()
+
+# List of origins that should be allowed to make cross-origin requests
+origins = [
+    "http://localhost:3000",  # Frontend running on localhost:3000
+    "http://localhost:8080",  # Adjust with your frontend's URL and port
+    "http://your-frontend-domain.com"  # Add other origins as needed
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # Or specify allowed methods like ["GET", "POST"]
+    allow_headers=["*"],  # Or specify allowed headers
+)
+
+app.include_router(root_router)
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8000, debug=True)

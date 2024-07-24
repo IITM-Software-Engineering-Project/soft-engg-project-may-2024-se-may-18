772b3f3c0330dfda45ec89a6f282725ac8f83362
nailsonseat <aadarshv3@gmail.com>
2024-07-25 01:46:34 +0530

    Setup router and vuetify

--- seek_next/src/main.ts
+++ seek_next/src/main.ts
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import router from './routes/router.ts'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
-createApp(App).mount('#app')
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+createApp(App).use(router).use(vuetify).mount('#app')

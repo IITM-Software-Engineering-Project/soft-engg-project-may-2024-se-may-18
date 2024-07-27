7fbcc2daccf8bcfba3c9736a373b8a4cd2baaecc
nailsonseat <aadarshv3@gmail.com>
2024-07-27 14:19:32 +0530

    create tables if they dont exist

--- server/database/models.py
+++ server/database/models.py
@@ -1,16 +1,25 @@
-from sqlalchemy import Column, Integer, String, Timestamp
+from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
+from database.db_sql import init_db
 
-db = declarative_base()
+Base = declarative_base()
 
 
-class User(db):
+class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
     username = Column(String(50), nullable=False)
     role = Column(String(50), nullable=False)
-    last_login = Column(Timestamp, nullable=False)
+    last_login = Column(DateTime, nullable=False)
     email = Column(String(100), nullable=False)
     password = Column(String(100), nullable=False)
+
+
+engine = None
+
+if not engine:
+    engine = init_db()
+    Base.metadata.create_all(engine, checkfirst=True)
+    session = sessionmaker(bind=engine, expire_on_commit=True)

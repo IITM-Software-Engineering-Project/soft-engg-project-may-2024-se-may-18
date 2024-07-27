9aef92c90245eb6b7acba0758907d922d9bcd832
nailsonseat <aadarshv3@gmail.com>
2024-07-27 14:26:39 +0530

    Handle db uri properly

--- server/database/db_sql.py
+++ server/database/db_sql.py
@@ -9,7 +9,8 @@ load_dotenv()
 def init_db():
     try:
         print("Connecting to SQL database.")
-        engine = create_engine(os.getenv('DATABASE_SQL_URL'))
+        engine = create_engine(os.getenv('DATABASE_SQL_URL').replace(
+            "postgresql://", "cockroachdb://"))
         return engine
     except Exception as e:
         print("Failed to connect to database.")

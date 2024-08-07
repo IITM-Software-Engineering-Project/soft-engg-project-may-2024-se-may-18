336afb3ea5f26fbe1300f37fe5e8a466a2135df4
nailsonseat <aadarshv3@gmail.com>
2024-08-07 14:19:29 +0530

    defined store in main.ts

--- seek_next/src/main.ts
+++ seek_next/src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routes/router.ts'
+import { store, key } from './store/Store.ts'
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
@@ -11,4 +12,4 @@ const vuetify = createVuetify({
     directives,
 })
 
-createApp(App).use(router).use(vuetify).mount('#app')
+createApp(App).use(router).use(store, key).use(vuetify).mount('#app')

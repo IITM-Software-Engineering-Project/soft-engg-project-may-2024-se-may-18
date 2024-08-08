663300eb63defb4597ed012e4436c904e9f6473e
nailsonseat <aadarshv3@gmail.com>
2024-08-08 19:46:18 +0530

    update vuex related shims

---
+++ seek_next/src/store-shims.d.ts
@@ -0,0 +1,9 @@
+
+import { ComponentCustomProperties } from 'vue'
+import { Store } from 'vuex'
+
+declare module '@vue/runtime-core' {
+    interface ComponentCustomProperties {
+        $store: Store<State>
+    }
+}
--- seek_next/src/vuex-shims.d.ts
+++ seek_next/src/vuex-shims.d.ts
@@ -11,17 +11,3 @@ declare module "vuex" {
     export * from "vuex/types/logger.d.ts";
     export * from "vuex/types/vue.d.ts";
 }
-
-// import { ComponentCustomProperties } from 'vue'
-// import { Store } from 'vuex'
-
-// declare module '@vue/runtime-core' {
-//     // Declare your own store states.
-//     interface State {
-//         count: number
-//     }
-
-//     interface ComponentCustomProperties {
-//         $store: Store<State>
-//     }
-// }

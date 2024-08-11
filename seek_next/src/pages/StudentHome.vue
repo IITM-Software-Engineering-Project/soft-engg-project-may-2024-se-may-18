37627fcfa0191050aa0cd7fd890693d6ba7b90c7
harishsahadev <harishsahadev@gmail.com>
2024-08-11 08:48:43 +0530

    chore: Refactor StudentHome.vue for better course display

--- seek_next/src/pages/StudentHome.vue
+++ seek_next/src/pages/StudentHome.vue
@@ -4,48 +4,58 @@
       <v-col>
         <h2>Your Enrolled Courses</h2>
         <!-- List of Enrolled Courses -->
-        <v-list dense>
-          <v-list-item v-for="course in enrolledCourses" :key="course.id">
-            <v-list-item-content>
-              <v-list-item-title>{{ course.title }}</v-list-item-title>
-              <v-list-item-subtitle>{{ course.description }}</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
+        <v-list >
+            <v-list-item v-for="course in enrolledCourses" :key="course['id']">
+              <v-list-item-title>{{ course['title'] }}</v-list-item-title>
+      
+            </v-list-item>
+            <v-list-item v-if="enrolledCourses.length === 0">
+              <v-list-item-title>No courses enrolled</v-list-item-title>
+            </v-list-item>
         </v-list>
+    
         <!-- Button to View All Courses -->
         <v-btn @click="goToAllCourses" color="primary" class="mt-4">
           View All Courses
         </v-btn>
+        <v-btn @click="logout">Logout</v-btn>
       </v-col>
     </v-row>
   </v-container>
+
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted } from 'vue';
-import { useStore } from 'vuex';
-import { State } from '../store/Store.ts'; // Import the State interface
+import { mapGetters } from 'vuex'
 
-export default defineComponent({
+
+export default {
   name: 'student-home',
-  setup() {
-    const store = useStore<State>();
-    const studentId = '11'; // Replace with dynamic student ID if needed
-
-    onMounted(() => {
-      store.dispatch('fetchEnrolledCourses', studentId);
-    });
-
+  data() {
     return {
-      enrolledCourses: store.getters.enrolledCourses,
-    };
+      studentId : '11',
+    }
+  },
+
+  mounted() {
+      this.$store.dispatch('fetchEnrolledCourses', this.studentId);
+  },
+
+  computed: {
+    ...mapGetters({
+      enrolledCourses: 'enrolledCourses',
+  }) ,
   },
   methods: {
     goToAllCourses() {
       this.$router.push('/all-courses');
     },
+    logout() {
+      this.$store.dispatch('signOut')
+    }
   },
-});
+  
+};
 </script>
 
 <style scoped>

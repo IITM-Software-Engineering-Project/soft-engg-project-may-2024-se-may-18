0671f05f6643ab5b686ab2e9ed74301b152a43a3
harishsahadev <harishsahadev@gmail.com>
2024-08-15 16:38:20 +0530

    feat: Update AI search and explain endpoints to use database

--- server/resources/gen_ai.py
+++ server/resources/gen_ai.py
@@ -1,13 +1,26 @@
 from typing import List, Optional
-from fastapi import APIRouter, File, Request, UploadFile
+from fastapi import APIRouter, File, Request, UploadFile, Depends
 from constants.prompts import search_courses_prompt, explain_courses_prompt, programming_feedback_prompt
 from dotenv import load_dotenv
 from api.gemini import call_gemini, call_gemini_vision
 from fastapi import HTTPException
 
+from sqlalchemy.orm import sessionmaker, Session
+from database.models import Course
+from database.db_sql import init_db
+
 load_dotenv()
 
 genai_router = APIRouter()
+
+def get_db():
+    engine = init_db()
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @genai_router.post("/ai-search-courses",
@@ -15,7 +28,7 @@ genai_router = APIRouter()
                    response_description="Response from AI in the form of a json",
                    tags=["Gen AI"],
                    )
-async def gemini(request: Request):
+async def gemini(request: Request, session: Session = Depends(get_db)):
     data = await request.json()
     if "prompt" not in data.keys():
         raise HTTPException(status_code=422, detail="Prompt is required")
@@ -24,18 +37,46 @@ async def gemini(request: Request):
     try:
         data = data["data"]
     except Exception:
-        data = None
+        # data = None
+        x  = session.query(Course).all()
+
+        data = []
+        for i in x:
+            data.append({
+                "course_id": i.id,
+                "course_title": i.title,
+                "course_description": i.description
+            })
 
     response = call_gemini(prompt, data)
     return response
 
+
+# @genai_router.post("/ai-explain-course",
+#                    description="Answer queston for a single course using generative AI",
+#                    response_description="Response from AI in the form of a json with a text message inside",
+#                    tags=["Gen AI"],
+#                    )
+# async def gemini(request: Request):
+#     data = await request.json()
+#     if "prompt" not in data.keys():
+#         raise HTTPException(status_code=422, detail="Prompt is required")
+#     prompt = data["prompt"]
+#     prompt = explain_courses_prompt.format(user_prompt=prompt)
+#     try:
+#         data = data["data"]
+#     except Exception:
+#         data = None
+
+#     response = call_gemini(prompt, data)
+#     return response
 
 @genai_router.post("/ai-explain-course",
                    description="Answer queston for a single course using generative AI",
                    response_description="Response from AI in the form of a json with a text message inside",
                    tags=["Gen AI"],
                    )
-async def gemini(request: Request):
+async def gemini(request: Request, session: Session = Depends(get_db)):
     data = await request.json()
     if "prompt" not in data.keys():
         raise HTTPException(status_code=422, detail="Prompt is required")
@@ -44,7 +85,16 @@ async def gemini(request: Request):
     try:
         data = data["data"]
     except Exception:
-        data = None
+        # data = None
+        x  = session.query(Course).all()
+
+        data = []
+        for i in x:
+            data.append({
+                "course_id": i.id,
+                "course_title": i.title,
+                "course_description": i.description
+            })
 
     response = call_gemini(prompt, data)
     return response

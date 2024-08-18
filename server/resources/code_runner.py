1ea59821b46d0ba686ab1e2191da479b6f1df65c
nailsonseat <aadarshv3@gmail.com>
2024-08-18 22:33:32 +0530

    fixed error

--- server/resources/code_runner.py
+++ server/resources/code_runner.py
@@ -1,14 +1,13 @@
+import os
 from fastapi import APIRouter, HTTPException, Request, Query
 from database.no_sql import get_user_code_collection, get_code_info_collection
 from pydantic import BaseModel
 from api.payload_schema.payloadschema import CodeInfo
 import requests
+from dotenv import load_dotenv
 
+load_dotenv()
 code_runner_router = APIRouter()
-
-# Replace these with your JDoodle credentials
-CLIENT_ID = "d5074dbfe7c2103094a5e1f672956a19"
-CLIENT_SECRET = "d0b13c44223cda1e0a6e97d8c7bea5f581d6c46de7a6820e0a3e28537ebcc839"
 
 
 class CodeExecutionRequest(BaseModel):
@@ -17,34 +16,105 @@ class CodeExecutionRequest(BaseModel):
     versionIndex: str
 
 
+@code_runner_router.get("/get-code-problems/{module_id}",
+                        description="Get code assignments from the database",
+                        response_description="Code assignments retrieved successfully",
+                        tags=["Coding Assignment", "Instructor"])
+async def get_code_assignments(module_id: str):
+    # Retrieve the user code collection
+    user_code_collection = get_code_info_collection()
+
+    # Define the filter to find the documents by assignment_id
+    filter_query = {
+        "module_id": module_id
+    }
+
+    # Retrieve the code assignments for the given assignment_id
+    code_assignments = user_code_collection.find(filter_query)
+
+    # Check if any code assignments were found
+    if code_assignments == None:
+        raise HTTPException(
+            status_code=404, detail="Code assignments not found")
+
+    # Prepare the response data
+    response_data = []
+    for code_assignment in code_assignments:
+        response_data.append({
+            "module_id": code_assignment["module_id"],
+            "problem_id": code_assignment["problem_id"],
+            "problem_statement": code_assignment["problem_statement"],
+            "total_test_cases": code_assignment["total_test_cases"],
+            "test_cases": code_assignment["test_cases"]
+        })
+
+    return response_data
+
+
+@code_runner_router.get("/get-code-problem/{problem_id}",
+                        description="Get code assignment from the database",
+                        response_description="Code assignment retrieved successfully",
+                        tags=["Coding Assignment", "Instructor"])
+async def get_code_assignment(problem_id: str):
+    # Retrieve the user code collection
+    user_code_collection = get_code_info_collection()
+
+    # Define the filter to find the documents by assignment_id
+    filter_query = {
+        "problem_id": problem_id
+    }
+
+    # Retrieve the code assignments for the given assignment_id
+    code_assignment = user_code_collection.find_one(filter_query)
+
+    # Check if any code assignments were found
+    if code_assignment == None:
+        raise HTTPException(
+            status_code=404, detail="Code assignment not found")
+
+    # Prepare the response data
+    response_data = {
+        "module_id": code_assignment["module_id"],
+        "problem_id": code_assignment["problem_id"],
+        "problem_statement": code_assignment["problem_statement"],
+        "total_test_cases": code_assignment["total_test_cases"],
+        "test_cases": code_assignment["test_cases"]
+    }
+
+    return response_data
+
+
 @code_runner_router.post("/execute-code")
 async def execute_code(request: Request):
     data = await request.json()
     # print(data)
-    
+
     # Retrieve the CodeInfo document for the given problem_id
     code_info_collection = get_code_info_collection()
-    code_info = code_info_collection.find_one({"problem_id": data["problem_id"]})
-    
+    code_info = code_info_collection.find_one(
+        {"problem_id": data["problemId"]})
+
     if not code_info:
         raise HTTPException(status_code=404, detail="Problem not found")
-    
+
     test_results = []
-    
+
     for test_case in code_info['test_cases']:
         execution_data = {
-            "clientId": CLIENT_ID,
-            "clientSecret": CLIENT_SECRET,
+            "clientId": os.getenv('JDOODLE_CLIENT_ID'),
+            "clientSecret": os.getenv('JDOODLE_CLIENT_SECRET'),
             "script": data["code"],
             "language": data["language"],
-            "versionIndex": data.get("versionIndex", "0"),  # Default version index if not provided
+            "versionIndex": data.get("versionIndex", "0"),
             "stdin": test_case["input"]
         }
 
-        response = requests.post("https://api.jdoodle.com/v1/execute", json=execution_data)
+        response = requests.post(
+            "https://api.jdoodle.com/v1/execute", json=execution_data)
 
         if response.status_code != 200:
-            raise HTTPException(status_code=response.status_code, detail="Failed to execute code")
+            raise HTTPException(status_code=response.status_code,
+                                detail="Failed to execute code")
 
         actual_output = response.json().get("output", "").strip()
         expected_output = test_case["expected_output"].strip()
@@ -55,14 +125,14 @@ async def execute_code(request: Request):
             "actual_output": actual_output,
             "passed": actual_output == expected_output
         })
-    
+
     return {"results": test_results}
 
 
 @code_runner_router.post("/add-code-info",
-                  description="Add or update code information in the database",
-                  response_description="Code information added or updated successfully",
-                  tags=["Coding Assignment", "Instructor"])
+                         description="Add or update code information in the database",
+                         response_description="Code information added or updated successfully",
+                         tags=["Coding Assignment", "Instructor"])
 async def add_code_info(request: CodeInfo):
     data = request.model_dump()  # Convert Pydantic model to dictionary
 
@@ -98,9 +168,9 @@ async def add_code_info(request: CodeInfo):
 
 
 @code_runner_router.delete("/delete-code-info",
-                    description="Delete code information from the database",
-                    response_description="Code information deleted successfully",
-                    tags=["Coding Assignment","Instructor"])
+                           description="Delete code information from the database",
+                           response_description="Code information deleted successfully",
+                           tags=["Coding Assignment", "Instructor"])
 async def delete_code_info(problem_id: str = Query(..., min_length=1, description="The ID of the problem to delete")):
     # data = request.model_dump()  # Convert Pydantic model to dictionary
     if not problem_id.strip():
@@ -126,4 +196,3 @@ async def delete_code_info(problem_id: str = Query(..., min_length=1, descriptio
     return {
         "message": "Code information deleted successfully"
     }
-
